6a385758798e9f324cad5c325f6556d218f95a77
Paul Leopardi <paul.leopardi@anu.edu.au>
2025-07-03 16:00:23 +1000

    Clean up the Python code to satisfy flake8

--- CMIP7/esm1p6/atmosphere/aerosol/cmip7_aerosol_common.py
+++ CMIP7/esm1p6/atmosphere/aerosol/cmip7_aerosol_common.py
@@ -4,19 +4,28 @@ import ants.io
 import ants.io.save as save
 import mule
 import iris
-import cftime, cf_units
-from pathlib import Path
-import os, tempfile
+import cftime
+import cf_units
+import os
+import tempfile
 
-from cmip7_ancil_constants import * 
-from cmip7_ancil_paths import *
+from pathlib import Path
+
+from cmip7_ancil_constants import (
+        CMIP7_PI_YEAR,
+        UM_VERSION)
+from cmip7_ancil_paths import (
+        ANCIL_TARGET_PATH,
+        ESM16_GRID_MASK_FILE)
 
 CMIP7_AEROSOL_SAVE_DIR = Path(ANCIL_TARGET_PATH) / 'esm16_aerosols'
 # Ensure that the directory exists.
 os.makedirs(CMIP7_AEROSOL_SAVE_DIR, mode=0o755, exist_ok=True)
 
+
 def cmip7_aerosol_save_path(filename):
     return str(Path(CMIP7_AEROSOL_SAVE_DIR) / filename)
+
 
 mask_esm15 = iris.load_cube(ESM16_GRID_MASK_FILE)
 mask_esm15.coord('latitude').guess_bounds()
@@ -25,20 +34,23 @@ mask_esm15.coord('longitude').guess_bounds()
 interpolation_scheme = iris.analysis.AreaWeighted(mdtol=0.5)
 
 # For CMIP6 and CMIP7 data
-t1 = cftime.DatetimeNoLeap(1850,1,1)
-t2 = cftime.DatetimeNoLeap(1850,12,31)
-constraint_1850 = iris.Constraint(time=lambda cell: t1 <= cell.point < t2)
+CMIP7_PI_BEG_DATE = cftime.DatetimeNoLeap(CMIP7_PI_YEAR, 1, 1)
+CMIP7_PI_END_DATE = cftime.DatetimeNoLeap(CMIP7_PI_YEAR, 12, 31)
+CMIP7_PI_DATE_CONSTRAINT = iris.Constraint(
+        time=lambda cell: CMIP7_PI_BEG_DATE <= cell.point < CMIP7_PI_END_DATE)
+
 
 def zero_poles(cube):
     # Polar values should have no longitude dependence
     # For aerosol emissions they should be zero
     latdim = cube.coord_dims('latitude')
     assert latdim == (1,)
-    cube.data[:,0] = 0.
-    cube.data[:,-1] = 0.
+    cube.data[:, 0] = 0.
+    cube.data[:, -1] = 0.
 
-# Change the calendar to Gregorian for the model
+
 def set_gregorian(var):
+    # Change the calendar to Gregorian for the model
     time = var.coord('time')
     origin = time.units.origin
     newunits = cf_units.Unit(origin, calendar='proleptic_gregorian')
@@ -47,35 +59,49 @@ def set_gregorian(var):
     tbnds = np.array(time.bounds)
     for i in range(len(time.points)):
         date = time.units.num2date(tvals[i])
-        newdate = cftime.DatetimeProlepticGregorian(date.year, date.month, date.day, date.hour, date.minute, date.second)
+        newdate = cftime.DatetimeProlepticGregorian(
+            date.year,
+            date.month,
+            date.day,
+            date.hour,
+            date.minute,
+            date.second)
         tvals[i] = newunits.date2num(newdate)
         for j in range(2):
             date = time.units.num2date(tbnds[i][j])
-            newdate = cftime.DatetimeProlepticGregorian(date.year, date.month, date.day, date.hour, date.minute, date.second)
+            newdate = cftime.DatetimeProlepticGregorian(
+                date.year,
+                date.month,
+                date.day,
+                date.hour,
+                date.minute,
+                date.second)
             tbnds[i][j] = newunits.date2num(newdate)
     time.points = tvals
     time.bounds = tbnds
     time.units = newunits
 
+
 def set_coord_system(cube):
-    coord_system = iris.coord_systems.GeogCS(6371229.0) 
+    coord_system = iris.coord_systems.GeogCS(6371229.0)
     cube.coord('latitude').coord_system = coord_system
     cube.coord('longitude').coord_system = coord_system
 
-def save_ancil(cubes, filename):
-    
-    # Single year creates file with correct time_type=2
-    # ants creates files with the model_version header set to the ants version. UM vn7.3
-    # interprets 201 as an old unsupported dump format.
-    # Need to reset to 703
 
+def save_ancil(cubes, filename):
+    """
+    Single year creates file with correct time_type=2
+    ants creates files with the model_version header set to the ants version.
+    UM vn7.3 interprets 201 as an old unsupported dump format.
+    Need to reset to 703.
+    """
     ants.__version__ = UM_VERSION
     # Handle both a list and a single cube
     if not type(cubes) is list:
         cubes = [cubes]
     for cube in cubes:
-        cube.attributes["grid_staggering"] = 3 # New dynamics
-        cube.attributes["time_type"] = 1 # Gregorian
+        cube.attributes["grid_staggering"] = 3  # New dynamics
+        cube.attributes["time_type"] = 1  # Gregorian
         set_gregorian(cube)
     # ants doesn't set the calendar header for monthly fields
     # See fileformats/ancil/time_headers.py
@@ -89,6 +115,9 @@ def save_ancil(cubes, filename):
         ff.fixed_length_header.calendar = 1
         ff.to_file(filename)
 
+
 def fix_coords(cube):
-    cube.coord('latitude').coord_system = mask_esm15.coord('latitude').coord_system
-    cube.coord('longitude').coord_system = mask_esm15.coord('longitude').coord_system
+    cube.coord('latitude').coord_system = (
+        mask_esm15.coord('latitude').coord_system)
+    cube.coord('longitude').coord_system = (
+        mask_esm15.coord('longitude').coord_system)
