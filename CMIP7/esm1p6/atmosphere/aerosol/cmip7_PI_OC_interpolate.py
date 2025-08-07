7d1451ccbf2957b18ae18e2c8373f3513cf38b3c
Paul Leopardi <paul.leopardi@anu.edu.au>
2025-08-07 16:52:12 +1000

    Add solar forcings and use arguments

--- CMIP7/esm1p6/atmosphere/aerosol/cmip7_PI_OC_interpolate.py
+++ CMIP7/esm1p6/atmosphere/aerosol/cmip7_PI_OC_interpolate.py
@@ -1,8 +1,14 @@
 # Interpolate CMIP7 PI OCFF emissions to ESM1-6 grid
 from aerosol.cmip7_PI_aerosol_anthro import (
+        parse_args,
         cmip7_pi_aerosol_anthro_interpolate)
 
-cmip7_pi_aerosol_anthro_interpolate(
-        'OC',
-        stash_item=135,
-        ancil_filename='OCFF_1850_cmip7.anc')
+
+SAVE_FILENAME = 'OCFF_1850_cmip7.anc'
+
+
+if __name__ == '__main__':
+
+    args = parse_args(species='BC', save_filename=SAVE_FILENAME)
+
+    cmip7_pi_aerosol_anthro_interpolate(args, species='OC', stash_item=135)
