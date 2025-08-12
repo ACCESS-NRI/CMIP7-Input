ee693f74b38399a91d0c9f09b64f5ce9d8faee04
Paul Leopardi <paul.leopardi@anu.edu.au>
2025-08-12 16:51:52 +1000

    Revert "Add solar forcings and use arguments (#73)" (#74)
    This reverts commit 043c58ec83f182ea86c7019aea60804edf97343b.

--- CMIP7/esm1p6/atmosphere/aerosol/cmip7_HI_Bio_interpolate.py
+++ CMIP7/esm1p6/atmosphere/aerosol/cmip7_HI_Bio_interpolate.py
@@ -1,64 +1,14 @@
-# Interpolate CMIP7 HI Biomass burning emissions to ESM1.6 grid
+# Interpolate CMIP7 PI Biomass burning emissions to ESM1.6 grid
 
-from aerosol.cmip7_aerosol_biomass import (
-        load_cmip7_aerosol_biomass,
-        load_cmip7_aerosol_biomass_list,
-        save_cmip7_aerosol_biomass)
-from aerosol.cmip7_HI_aerosol import (
-        CMIP7_HI_AEROSOL_BEG_YEAR,
-        CMIP7_HI_AEROSOL_END_YEAR,
-        esm_hi_aerosol_save_dirpath)
-
-from cmip7_ancil_argparse import (
-        common_parser,
-        constraint_year_parser,
-        percent_parser)
-from cmip7_ancil_common import cmip7_date_constraint_from_args
-
-from argparse import ArgumentParser
+from aerosol.cmip7_HI_aerosol import ESM_HI_AEROSOL_SAVE_DIR
+from aerosol.cmip7_HI_aerosol_biomass import (
+        load_cmip7_hi_aerosol_biomass,
+        load_cmip7_hi_aerosol_biomass_percentage)
+from aerosol.cmip7_aerosol_biomass import save_cmip7_aerosol_biomass
 
 
-def parse_args():
-
-    SAVE_FILENAME = 'Bio_1849_2015_cmip7.anc'
-
-    parser = ArgumentParser(
-            prog='cmip7_HI_Bio_interpolate',
-            description=(
-                'Generate input files from CMIP7 historical biomass forcings'),
-            parents=[
-                common_parser(),
-                percent_parser(),
-                constraint_year_parser(
-                    beg_year=CMIP7_HI_AEROSOL_BEG_YEAR,
-                    end_year=CMIP7_HI_AEROSOL_END_YEAR)])
-    parser.add_argument('--dataset-date-range-list', type=eval)
-    parser.add_argument('--save-filename', default=SAVE_FILENAME)
-    return parser.parse_args()
-
-
-def load_cmip7_hi_aerosol_biomass(args, species):
-    return load_cmip7_aerosol_biomass_list(
-            args,
-            species,
-            args.dataset_date_range_list,
-            cmip7_date_constraint_from_args(args))
-
-
-def load_cmip7_hi_aerosol_biomass_percentage(args, species):
-    return load_cmip7_aerosol_biomass(
-            args,
-            species,
-            args.percent_date_range,
-            cmip7_date_constraint_from_args(args))
-
-
-if __name__ == '__main__':
-
-    args = parse_args()
-
-    save_cmip7_aerosol_biomass(
-            args,
-            load_cmip7_hi_aerosol_biomass_percentage,
-            load_cmip7_hi_aerosol_biomass,
-            esm_hi_aerosol_save_dirpath(args))
+save_cmip7_aerosol_biomass(
+        load_cmip7_hi_aerosol_biomass_percentage,
+        load_cmip7_hi_aerosol_biomass,
+        ESM_HI_AEROSOL_SAVE_DIR,
+        'Bio_1849_2015_cmip7.anc')
--- CMIP7/esm1p6/atmosphere/cmip7_ancil_constants.py
+++ CMIP7/esm1p6/atmosphere/cmip7_ancil_constants.py
@@ -1,4 +1,1 @@
-from datetime import datetime
-
-ANCIL_TODAY = datetime.now().strftime('%Y.%m.%d')
 UM_VERSION = '7.3'
