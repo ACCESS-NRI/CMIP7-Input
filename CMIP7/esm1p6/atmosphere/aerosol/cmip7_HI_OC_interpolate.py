8bc9ade00404cc50f6e0b9e70bf3694698a7595a
Paul Leopardi <paul.leopardi@anu.edu.au>
2025-08-12 16:51:52 +1000

    Revert "Add solar forcings and use arguments (#73)" (#74)
    This reverts commit 043c58ec83f182ea86c7019aea60804edf97343b.

--- CMIP7/esm1p6/atmosphere/aerosol/cmip7_HI_OC_interpolate.py
+++ CMIP7/esm1p6/atmosphere/aerosol/cmip7_HI_OC_interpolate.py
@@ -1,14 +1,8 @@
 # Interpolate CMIP7 HI OCFF emissions to ESM1.6 grid
 from aerosol.cmip7_HI_aerosol_anthro import (
-        parse_args,
         cmip7_hi_aerosol_anthro_interpolate)
 
-
-SAVE_FILENAME = 'OCFF_1849_2015_cmip7.anc'
-
-
-if __name__ == '__main__':
-
-    args = parse_args(species='OC', save_filename=SAVE_FILENAME)
-
-    cmip7_hi_aerosol_anthro_interpolate(args, species='OC', stash_item=135)
+cmip7_hi_aerosol_anthro_interpolate(
+        'OC',
+        stash_item=135,
+        ancil_filename='OCFF_1849_2015_cmip7.anc')
--- CMIP7/esm1p6/atmosphere/aerosol/cmip7_PI_BC_interpolate.py
+++ CMIP7/esm1p6/atmosphere/aerosol/cmip7_PI_BC_interpolate.py
@@ -1,14 +1,8 @@
 # Interpolate CMIP7 PI BC emissions to ESM1-6 grid
 from aerosol.cmip7_PI_aerosol_anthro import (
-        parse_args,
         cmip7_pi_aerosol_anthro_interpolate)
 
-
-SAVE_FILENAME = 'BC_1850_cmip7.anc'
-
-
-if __name__ == '__main__':
-
-    args = parse_args(species='BC', save_filename=SAVE_FILENAME)
-
-    cmip7_pi_aerosol_anthro_interpolate(args, species='BC', stash_item=129)
+cmip7_pi_aerosol_anthro_interpolate(
+        'BC',
+        stash_item=129,
+        ancil_filename='BC_1850_cmip7.anc')
