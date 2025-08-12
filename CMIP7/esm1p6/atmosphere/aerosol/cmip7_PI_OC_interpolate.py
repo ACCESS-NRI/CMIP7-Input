fda3ff76c40e7f679f4d947977e3ba54191620d5
Paul Leopardi <paul.leopardi@anu.edu.au>
2025-08-12 17:08:37 +1000

    Fix a typo: BC should have been OC

--- CMIP7/esm1p6/atmosphere/aerosol/cmip7_PI_OC_interpolate.py
+++ CMIP7/esm1p6/atmosphere/aerosol/cmip7_PI_OC_interpolate.py
@@ -9,6 +9,6 @@ SAVE_FILENAME = 'OCFF_1850_cmip7.anc'
 
 if __name__ == '__main__':
 
-    args = parse_args(species='BC', save_filename=SAVE_FILENAME)
+    args = parse_args(species='OC', save_filename=SAVE_FILENAME)
 
     cmip7_pi_aerosol_anthro_interpolate(args, species='OC', stash_item=135)
