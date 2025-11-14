35ebbcbd94e11a07caf4e2239aef54782db7e6d8
Paul Leopardi <paul.leopardi@anu.edu.au>
2025-11-14 16:10:52 +1100

    Remove trailing whitespace

--- CMIP7/esm1p6/atmosphere/nitrogen/cmip7_nitrogen.py
+++ CMIP7/esm1p6/atmosphere/nitrogen/cmip7_nitrogen.py
@@ -3,7 +3,7 @@ from pathlib import Path
 NITROGEN_SPECIES = ("drynhx", "drynoy", "wetnhx", "wetnoy")
 """
 As per https://forum.access-hive.org.au/t/proposed-changes-to-variable-names-in-um-netcdf-conversion/4599
-m01s03i884 is NITROGEN DEPOSITION 
+m01s03i884 is NITROGEN DEPOSITION
 """
 NITROGEN_STASH_SECTION = 3
 NITROGEN_STASH_ITEM = 884
