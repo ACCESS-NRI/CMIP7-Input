5d55ed5fb115ef61745641b0617c2de847d035ab
Paul Leopardi <paul.leopardi@anu.edu.au>
2025-12-15 15:43:13 +1100

    Add a function that extends a cube time series

--- CMIP7/esm1p6/atmosphere/cmip7_HI.py
+++ CMIP7/esm1p6/atmosphere/cmip7_HI.py
@@ -1,8 +1,13 @@
 import calendar
 from pathlib import Path
 
+import iris
 import mule
-from cmip7_ancil_constants import ANCIL_TODAY, UM_VERSION
+from cmip7_ancil_constants import (
+    ANCIL_TODAY,
+    MONTHS_IN_A_YEAR,
+    UM_VERSION,
+)
 from cmip7_PI import CMIP7_PI_YEAR
 
 CMIP7_HI_BEG_YEAR = CMIP7_PI_YEAR
@@ -41,3 +46,35 @@ def fix_esm15_hi_ancil_date(ifile, ofile):
         field.lbdatd = calendar.monthrange(field.lbyr, field.lbmon)[1]
 
     ff.to_file(ofile)
+
+
+def extend_hi_years(cube):
+    """
+    Extend a cube representing a time series by duplicating and adjusting
+    the first and last years.
+    Based on code from ozone_cmip6_ancillary_for_suite.py by Steven Hardimann
+    of the UK Met Office.
+    """
+    time_coord = cube.coord("time")
+    time_points = time_coord.points
+    assert len(time_points) > MONTHS_IN_A_YEAR
+
+    # Duplicate the first year.
+    length_one_year = time_points[MONTHS_IN_A_YEAR] - time_points[0]
+    beg_year = cube[:MONTHS_IN_A_YEAR].copy()
+    beg_year_tc = beg_year.coord("time")
+    beg_year_tc.points -= length_one_year
+    if time_coord.has_bounds():
+        beg_year_tc.bounds -= length_one_year
+
+    # Duplicate the last year.
+    length_one_year = time_points[-1] - time_points[-1 - MONTHS_IN_A_YEAR]
+    end_year = cube[-MONTHS_IN_A_YEAR:].copy()
+    end_year_tc = end_year.coord("time")
+    end_year_tc.points += length_one_year
+    if time_coord.has_bounds():
+        end_year_tc.bounds += length_one_year
+
+    # Return a cube with extended years.
+    cubelist = iris.cube.CubeList((beg_year, cube, end_year))
+    return cubelist.concatenate_cube()
