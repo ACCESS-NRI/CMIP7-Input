1ca9f1e0ecb29825cd2bc92c9fb5669eff0913c5
Paul Leopardi <paul.leopardi@anu.edu.au>
2025-12-15 15:55:23 +1100

    First draft of historical nitrogen forcing script

--- CMIP7/esm1p6/atmosphere/nitrogen/cmip7_HI_nitrogen_generate.py
+++ CMIP7/esm1p6/atmosphere/nitrogen/cmip7_HI_nitrogen_generate.py
@@ -1,6 +1,15 @@
 from argparse import ArgumentParser
+from pathlib import Path
 
 from cmip7_ancil_argparse import common_parser
+from cmip7_ancil_constants import ANCIL_TODAY
+from cmip7_HI import extend_hi_years
+from nitrogen.cmip7_nitrogen import (
+    cmip7_nitrogen_dirpath,
+    load_cmip7_nitrogen,
+    regrid_cmip7_nitrogen,
+    save_cmip7_nitrogen,
+)
 
 
 def parse_args():
@@ -16,5 +25,35 @@ def parse_args():
     return parser.parse_args()
 
 
+def cmip7_hi_nitrogen_filepath(args, species):
+    dirpath = cmip7_nitrogen_dirpath(args, "mon", species)
+    filename = (
+        f"{species}_input4MIPs_surfaceFluxes_CMIP_"
+        f"{args.dataset_version}_gn_"
+        f"{args.dataset_date_range}.nc"
+    )
+    return dirpath / filename
+
+
+def esm_hi_nitrogen_save_dirpath(args):
+    return (
+        Path(args.ancil_target_dirname)
+        / "modern"
+        / "historical"
+        / "atmosphere"
+        / "land"
+        / "biogeochemistry"
+        / args.esm_grid_rel_dirname
+        / ANCIL_TODAY
+    )
+
+
 if __name__ == "__main__":
     args = parse_args()
+
+    # Load the CMIP7 datasets
+    nitrogen_cube = load_cmip7_nitrogen(args, cmip7_hi_nitrogen_filepath)
+    # Regrid to match the ESM1.5 mask and extend the time series
+    esm_cube = extend_hi_years(regrid_cmip7_nitrogen(args, nitrogen_cube))
+    # Save the ancillary
+    save_cmip7_nitrogen(args, esm_cube, esm_hi_nitrogen_save_dirpath)
