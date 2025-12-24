9514f6f25bdaab989407180953d07f1b8e531dad
Paul Leopardi <paul.leopardi@anu.edu.au>
2025-12-24 20:47:52 +1100

    Include aircraft emissions

--- CMIP7/esm1p6/atmosphere/co2/cmip7_EH_CO2_interpolate.py
+++ CMIP7/esm1p6/atmosphere/co2/cmip7_EH_CO2_interpolate.py
@@ -1,10 +1,18 @@
 # Interpolate CMIP7 EH CO2 emissions to ESM1.6 grid
 from pathlib import Path
 
-from aerosol.cmip7_aerosol_anthro import cmip7_aerosol_anthro_interpolate
+import iris
+from aerosol.cmip7_aerosol_common import zero_poles
 from aerosol.cmip7_HI_aerosol_anthro import (
+    load_cmip7_hi_aerosol_air_anthro,
     load_cmip7_hi_aerosol_anthro,
     parse_args,
+)
+from cmip7_ancil_common import (
+    INTERPOLATION_SCHEME,
+    esm_grid_mask_cube,
+    extend_years,
+    save_ancil,
 )
 from cmip7_ancil_constants import ANCIL_TODAY
 
@@ -25,13 +33,24 @@ def esm_eh_co2_save_dirpath(args):
 
 
 def cmip7_eh_co2_anthro_interpolate(args):
-    cmip7_aerosol_anthro_interpolate(
-        args,
-        load_cmip7_hi_aerosol_anthro,
-        SPECIES,
-        STASH_ITEM,
-        esm_eh_co2_save_dirpath(args),
+    cube = load_cmip7_hi_aerosol_anthro(args, SPECIES)
+    cube_sum = cube.collapsed(["sector"], iris.analysis.SUM)
+    cube_air = load_cmip7_hi_aerosol_air_anthro(args, SPECIES)
+    cube_air_sum = cube_air.collapsed(["altitude"], iris.analysis.SUM)
+    cube_tot = cube_sum + cube_air_sum
+
+    esm_cube = cube_tot.regrid(esm_grid_mask_cube(args), INTERPOLATION_SCHEME)
+    esm_cube.data = esm_cube.data.filled(0.0)
+    zero_poles(esm_cube)
+    esm_cube.attributes["STASH"] = iris.fileformats.pp.STASH(
+        model=1, section=0, item=STASH_ITEM
     )
+    # Extend the historical time series, if any,
+    # by duplicating the first and last years
+    esm_cube = extend_years(esm_cube)
+
+    save_dirpath = esm_eh_co2_save_dirpath(args)
+    save_ancil(esm_cube, save_dirpath, args.save_filename)
 
 
 if __name__ == "__main__":
