65d5bf132b8aa917414dd45157fb2707d5a56856
Paul Leopardi <paul.leopardi@anu.edu.au>
2025-12-25 00:11:04 +1100

    Read years 1849 and 2023 directly from the datasets

--- CMIP7/esm1p6/atmosphere/co2/cmip7_EH_CO2_interpolate.py
+++ CMIP7/esm1p6/atmosphere/co2/cmip7_EH_CO2_interpolate.py
@@ -11,13 +11,17 @@ from aerosol.cmip7_HI_aerosol_anthro import (
 from cmip7_ancil_common import (
     INTERPOLATION_SCHEME,
     esm_grid_mask_cube,
-    extend_years,
     save_ancil,
 )
 from cmip7_ancil_constants import ANCIL_TODAY
+from cmip7_HI import CMIP7_HI_BEG_YEAR, CMIP7_HI_END_YEAR
 
 SPECIES = "CO2"
 STASH_ITEM = 251
+# The CO2 time series includes 1849 and 2023,
+# so use these years directly from the datasets.
+CMIP7_HI_CO2_BEG_YEAR = CMIP7_HI_BEG_YEAR - 1
+CMIP7_HI_CO2_END_YEAR = CMIP7_HI_END_YEAR + 1
 
 
 def esm_eh_co2_save_dirpath(args):
@@ -33,9 +37,19 @@ def esm_eh_co2_save_dirpath(args):
 
 
 def cmip7_eh_co2_anthro_interpolate(args):
-    cube = load_cmip7_hi_aerosol_anthro(args, SPECIES)
+    cube = load_cmip7_hi_aerosol_anthro(
+        args,
+        SPECIES,
+        beg_year=CMIP7_HI_CO2_BEG_YEAR,
+        end_year=CMIP7_HI_CO2_END_YEAR,
+    )
     cube_sum = cube.collapsed(["sector"], iris.analysis.SUM)
-    cube_air = load_cmip7_hi_aerosol_air_anthro(args, SPECIES)
+    cube_air = load_cmip7_hi_aerosol_air_anthro(
+        args,
+        SPECIES,
+        beg_year=CMIP7_HI_CO2_BEG_YEAR,
+        end_year=CMIP7_HI_CO2_END_YEAR,
+    )
     cube_air_sum = cube_air.collapsed(["altitude"], iris.analysis.SUM)
     cube_tot = cube_sum + cube_air_sum
 
@@ -45,9 +59,6 @@ def cmip7_eh_co2_anthro_interpolate(args):
     esm_cube.attributes["STASH"] = iris.fileformats.pp.STASH(
         model=1, section=0, item=STASH_ITEM
     )
-    # Extend the historical time series, if any,
-    # by duplicating the first and last years
-    esm_cube = extend_years(esm_cube)
 
     save_dirpath = esm_eh_co2_save_dirpath(args)
     save_ancil(esm_cube, save_dirpath, args.save_filename)
