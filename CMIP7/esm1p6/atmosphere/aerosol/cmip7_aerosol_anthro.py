875bdaa119600aac39f7a2c9476d60b897a753ea
Paul Leopardi <paul.leopardi@anu.edu.au>
2025-12-24 20:59:51 +1100

    Fix a function name

--- CMIP7/esm1p6/atmosphere/aerosol/cmip7_aerosol_anthro.py
+++ CMIP7/esm1p6/atmosphere/aerosol/cmip7_aerosol_anthro.py
@@ -61,7 +61,7 @@ def load_cmip7_aerosol_anthro(args, species, date_range, constraint):
     return cube
 
 
-def load_cmip7_aerosol_anthro_air_list(
+def load_cmip7_aerosol_air_anthro_list(
     args, species, date_range_list, constraint
 ):
     cube = load_cmip7_aerosol_list(
