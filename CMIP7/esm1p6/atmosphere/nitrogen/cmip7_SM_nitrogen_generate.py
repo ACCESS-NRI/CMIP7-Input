c2927a01c774a986a097230ce5b9fac570df8a40
Paul Leopardi <paul.leopardi@anu.edu.au>
2026-04-28 17:20:05 +1000

    Generate ScenarioMIP nitrogen ancils

---
+++ CMIP7/esm1p6/atmosphere/nitrogen/cmip7_SM_nitrogen_generate.py
@@ -0,0 +1,60 @@
+from argparse import ArgumentParser
+from pathlib import Path
+
+from cmip7_ancil_argparse import common_parser
+from cmip7_ancil_common import extend_years
+from cmip7_ancil_constants import ANCIL_TODAY
+from nitrogen.cmip7_nitrogen import (
+    cmip7_nitrogen_dirpath,
+    load_cmip7_nitrogen,
+    regrid_cmip7_nitrogen,
+    save_cmip7_nitrogen,
+)
+
+
+def parse_args():
+    parser = ArgumentParser(
+        parents=[common_parser()],
+        prog="cmip7_SM_nitrogen_generate",
+        description=(
+            "Generate input files from CMIP7 ScenarioMIP nitrogen forcings"
+        ),
+    )
+    parser.add_argument("--scenario")
+    parser.add_argument("--dataset-date-range")
+    parser.add_argument("--save-filename")
+    return parser.parse_args()
+
+
+def cmip7_sm_nitrogen_filepath(args, species):
+    dirpath = cmip7_nitrogen_dirpath(args, "ScenarioMIP", "mon", species)
+    filename = (
+        f"{species}_input4MIPs_surfaceFluxes_ScenarioMIP_"
+        f"{args.dataset_version}_gn_"
+        f"{args.dataset_date_range}.nc"
+    )
+    return dirpath / filename
+
+
+def esm_sm_nitrogen_save_dirpath(args):
+    return (
+        Path(args.ancil_target_dirname)
+        / "scenarios"
+        / args.scenario
+        / "atmosphere"
+        / "land"
+        / "biogeochemistry"
+        / args.esm_grid_rel_dirname
+        / ANCIL_TODAY
+    )
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    # Load the CMIP7 datasets
+    nitrogen_cube = load_cmip7_nitrogen(args, cmip7_sm_nitrogen_filepath)
+    # Regrid to match the ESM1.5 mask and extend the time series
+    esm_cube = extend_years(regrid_cmip7_nitrogen(args, nitrogen_cube))
+    # Save the ancillary
+    save_cmip7_nitrogen(args, esm_cube, esm_sm_nitrogen_save_dirpath)
