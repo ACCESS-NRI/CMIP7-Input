8ed4b10c1756a33e46b9f05f1f10c2527b57e4f1
Spencer Wong <spencer.wong@anu.edu.au>
2026-05-13 16:17:16 +1000

    Formatting

--- CMIP7/esm1p6/atmosphere/co2/cmip7_F10CDR_CO2_generate.py
+++ CMIP7/esm1p6/atmosphere/co2/cmip7_F10CDR_CO2_generate.py
@@ -1,18 +1,17 @@
 import argparse
-import numpy as np
 from pathlib import Path
 
 import cf_units
 import cftime
 import iris
-
-from co2.cmip7_EH_CO2_interpolate import STASH_ITEM
+import numpy as np
 from cmip7_ancil_argparse import common_parser
 from cmip7_ancil_common import (
     esm_grid_mask_cube,
     save_ancil,
 )
 from cmip7_ancil_constants import ANCIL_TODAY
+from co2.cmip7_EH_CO2_interpolate import STASH_ITEM
 
 # Length of the declining emissions section
 INTERPOLATION_NYEARS = 100
@@ -21,7 +20,8 @@ FLAT_MINUS10_NYEARS = 100
 
 # CO2 flux (kg CO2 /m2/s) corresponding to emissions of 10PgC/year.
 # Taken from the flat10 ancillary:
-# /g/data/vk83/prerelease/configurations/inputs/access-esm1p6/modern/flat10/atmosphere/forcing/global.N96/2025.12.05/CO2_fluxes_flat10.anc
+# /g/data/vk83/prerelease/configurations/inputs/access-esm1p6/modern/flat10/
+# atmosphere/forcing/global.N96/2025.12.05/CO2_fluxes_flat10.anc
 PG10_C = np.float64(2.2802997268200897451606579124927520751953125e-09)
 
 
@@ -31,19 +31,19 @@ def parse_args():
         description=(
             "Generate CO2 emissions for the CMIP7 esm-flat10-cdr experiment"
         ),
-        parents=[common_parser()]
+        parents=[common_parser()],
     )
     parser.add_argument(
         "--year-start",
-        help="Initial year in file. Interpolation section will start from this year.",
+        help="Initial year in file.",
         required=True,
-        type=int
+        type=int,
     )
     parser.add_argument(
         "--nyear-zero",
         help="Number of years of zero emissions to add to the end.",
         required=True,
-        type=int
+        type=int,
     )
     parser.add_argument("--save-filename")
 
@@ -71,35 +71,40 @@ def create_f10cdr_data(nyear_zero, esm_latitude, esm_longitude):
     """
     # Emissions decline each January and remain flat for the rest of each year.
     yearly_vals = np.linspace(
-            start=PG10_C,
-            stop=-PG10_C,
-            num=INTERPOLATION_NYEARS + 1
+        start=PG10_C, stop=-PG10_C, num=INTERPOLATION_NYEARS + 1
     )
     decline_section = np.zeros(
-            shape=(INTERPOLATION_NYEARS * 12 + 1, esm_latitude.shape[0], esm_longitude.shape[0])
+        shape=(
+            INTERPOLATION_NYEARS * 12 + 1,
+            esm_latitude.shape[0],
+            esm_longitude.shape[0],
+        )
     )
 
     for i in range(decline_section.shape[0]):
-        decline_year = i//12
+        decline_year = i // 12
         decline_section[i, :, :] = np.full(
             shape=(esm_latitude.shape[0], esm_longitude.shape[0]),
-            fill_value=yearly_vals[decline_year]
+            fill_value=yearly_vals[decline_year],
         )
 
     flat_minus10_section = np.full(
-            shape=(FLAT_MINUS10_NYEARS * 12 - 1, esm_latitude.shape[0], esm_longitude.shape[0]),
-            fill_value=-PG10_C
+        shape=(
+            FLAT_MINUS10_NYEARS * 12 - 1,
+            esm_latitude.shape[0],
+            esm_longitude.shape[0],
+        ),
+        fill_value=-PG10_C,
     )
 
     zero_section = np.full(
-            shape=(nyear_zero*12, esm_latitude.shape[0], esm_longitude.shape[0]),
-            fill_value=0.0,
-            dtype=np.float64
+        shape=(nyear_zero * 12, esm_latitude.shape[0], esm_longitude.shape[0]),
+        fill_value=0.0,
+        dtype=np.float64,
     )
 
     flat10_cdr_data = np.concatenate(
-        [decline_section, flat_minus10_section, zero_section],
-        axis=0
+        [decline_section, flat_minus10_section, zero_section], axis=0
     )
 
     return flat10_cdr_data
@@ -117,29 +122,31 @@ def create_f10cdr_times(flat10_cdr_data, year_start):
     time_bnds = np.zeros((n_months, 2))
 
     for i in range(n_months):
-        year = year_start + i//12
+        year = year_start + i // 12
         month = i % 12 + 1
         date = cftime.DatetimeProlepticGregorian(year, month, 1)
         time_vals[i] = time_units.date2num(date)
         if month == 12:
-            next_date = cftime.DatetimeProlepticGregorian(year+1, 1, 1)
+            next_date = cftime.DatetimeProlepticGregorian(year + 1, 1, 1)
         else:
-            next_date = cftime.DatetimeProlepticGregorian(year, month+1, 1)
+            next_date = cftime.DatetimeProlepticGregorian(year, month + 1, 1)
 
         time_bnds[i, 0] = time_vals[i]
         time_bnds[i, 1] = time_units.date2num(next_date)
 
     time_coord = iris.coords.DimCoord(
-            time_vals,
-            var_name="time",
-            units=time_units,
-        )
+        time_vals,
+        var_name="time",
+        units=time_units,
+    )
     time_coord.bounds = time_bnds
 
     return time_coord
 
 
-def create_f10cdr_cube(flat10_cdr_data, flat10_cdr_times, esm_latitude, esm_longitude):
+def create_f10cdr_cube(
+    flat10_cdr_data, flat10_cdr_times, esm_latitude, esm_longitude
+):
     """
     Create an iris cube with data and coordinates for the flat10-cdr
     CO2 emissions.
@@ -167,9 +174,13 @@ def create_f10cdr_cube(flat10_cdr_data, flat10_cdr_times, esm_latitude, esm_long
 def cmip7_f10cdr_co2_generate(args):
     esm_longitude = esm_grid_mask_cube(args).coord("longitude")
     esm_latitude = esm_grid_mask_cube(args).coord("latitude")
-    f10cdr_data = create_f10cdr_data(args.nyear_zero, esm_latitude, esm_longitude)
+    f10cdr_data = create_f10cdr_data(
+        args.nyear_zero, esm_latitude, esm_longitude
+    )
     f10cdr_times = create_f10cdr_times(f10cdr_data, args.year_start)
-    f10cdr_cube = create_f10cdr_cube(f10cdr_data, f10cdr_times, esm_latitude, esm_longitude)
+    f10cdr_cube = create_f10cdr_cube(
+        f10cdr_data, f10cdr_times, esm_latitude, esm_longitude
+    )
     save_dirpath = f10cdr_co2_save_dirpath(args)
     save_ancil(f10cdr_cube, save_dirpath, args.save_filename)
 
