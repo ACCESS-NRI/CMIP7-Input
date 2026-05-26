ac863a4c74bba7743bdae743f4b0070296ac1370
Paul Leopardi <paul.leopardi@anu.edu.au>
2026-05-26 15:28:59 +1000

    Refactor volcanic forcings and add ScenarioMIP

---
+++ CMIP7/esm1p6/atmosphere/cmip7_SM.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+from cmip7_ancil_constants import ANCIL_TODAY
+
+CMIP7_SM_BEG_YEAR = 2025
+# Model time interpolation requires an extra year
+CMIP7_SM_END_YEAR = 2100
+CMIP7_SM_NBR_YEARS = CMIP7_SM_END_YEAR + 1 - CMIP7_SM_BEG_YEAR
+
+
+def esm_sm_forcing_save_dirpath(args):
+    return (
+        Path(args.ancil_target_dirname)
+        / "scenarios"
+        / args.scenario
+        / "atmosphere"
+        / "forcing"
+        / "resolution_independent"
+        / ANCIL_TODAY
+    )
--- CMIP7/esm1p6/atmosphere/volcanic/cmip7_SM_volcanic_generate.py
+++ CMIP7/esm1p6/atmosphere/volcanic/cmip7_SM_volcanic_generate.py
@@ -1,38 +1,39 @@
 from argparse import ArgumentParser
 
 from cmip7_ancil_argparse import dataset_parser, path_parser
-from cmip7_HI import esm_hi_forcing_save_dirpath
+from cmip7_SM import esm_sm_forcing_save_dirpath
 from volcanic.cmip7_volcanic import (
     cmip7_volcanic_dirpath,
     save_stratospheric_aerosol_optical_depth,
 )
 
-CMIP7_HI_VOLCANIC_BEG_YEAR = 1850
-CMIP7_HI_VOLCANIC_END_YEAR = 2023
+CMIP7_SM_VOLCANIC_BEG_YEAR = 2022
+CMIP7_SM_VOLCANIC_END_YEAR = 2100
 
 
 def parse_args():
     parser = ArgumentParser(
-        prog="cmip7_HI_volcanic_generate",
+        prog="cmip7_SM_volcanic_generate",
         description=(
-            "Generate input files from CMIP7 historical volcanic forcings"
+            "Generate input files from CMIP7 ScenarioMIP volcanic forcings"
         ),
         parents=[path_parser(), dataset_parser()],
     )
+    parser.add_argument("--scenario")
     parser.add_argument("--dataset-date-range")
     parser.add_argument("--save-filename")
     return parser.parse_args()
 
 
-def cmip7_hi_volcanic_filename(args):
+def cmip7_sm_volcanic_filename(args):
     return (
-        f"ext_input4MIPs_aerosolProperties_CMIP_"
+        f"ext_input4MIPs_aerosolProperties_ScenarioMIP_"
         f"{args.dataset_version}_gnz_"
         f"{args.dataset_date_range}.nc"
     )
 
 
-def save_hi_stratospheric_aerosol_optical_depth(args, dataset_path):
+def save_sm_stratospheric_aerosol_optical_depth(args, dataset_path):
     """
     Calculate the average stratospheric aerosol optical depth (SAOD)
     for each historical month by averaging extinction over latitude,
@@ -40,19 +41,19 @@ def save_hi_stratospheric_aerosol_optical_depth(args, dataset_path):
     """
     save_stratospheric_aerosol_optical_depth(
         args,
-        CMIP7_HI_VOLCANIC_BEG_YEAR,
-        CMIP7_HI_VOLCANIC_END_YEAR,
+        CMIP7_SM_VOLCANIC_BEG_YEAR,
+        CMIP7_SM_VOLCANIC_END_YEAR,
         dataset_path,
-        esm_hi_forcing_save_dirpath(args),
+        esm_sm_forcing_save_dirpath(args),
     )
 
 
 if __name__ == "__main__":
     args = parse_args()
 
-    dirpath = cmip7_volcanic_dirpath(args, "CMIP", "mon")
-    filename = cmip7_hi_volcanic_filename(args)
+    dirpath = cmip7_volcanic_dirpath(args, "ScenarioMIP", "mon")
+    filename = cmip7_sm_volcanic_filename(args)
     dataset_path = dirpath / filename
 
     # Calculate and save the average stratospheric aerosol optical depth.
-    save_hi_stratospheric_aerosol_optical_depth(args, dataset_path)
+    save_sm_stratospheric_aerosol_optical_depth(args, dataset_path)
