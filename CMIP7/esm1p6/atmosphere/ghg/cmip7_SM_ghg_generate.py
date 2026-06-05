96d26d03626bec596f4699dd1bde8f19b36b2a76
Paul Leopardi <paul.leopardi@anu.edu.au>
2026-06-05 11:41:46 +1000

    Apply ruff formatting to satisfy pre-commit

--- CMIP7/esm1p6/atmosphere/ghg/cmip7_SM_ghg_generate.py
+++ CMIP7/esm1p6/atmosphere/ghg/cmip7_SM_ghg_generate.py
@@ -7,8 +7,8 @@ from cmip7_SM import (
 )
 from ghg.cmip7_ghg import GHG_MOLAR_MASS
 from ghg.cmip7_ghg_series import (
-    load_cmip7_ghg_series_mmr,
     cmip7_ghg_update_namelists_file,
+    load_cmip7_ghg_series_mmr,
 )
 
 
@@ -17,7 +17,8 @@ def parse_args():
         parents=[path_parser(), dataset_parser()],
         prog="cmip7_SM_ghg_generate",
         description=(
-            "Generate input files from CMIP7 ScenarioMIP greenhouse gas forcings"
+            "Generate input files from CMIP7 ScenarioMIP "
+            "greenhouse gas forcings"
         ),
     )
     parser.add_argument("--dataset-date-range")
@@ -30,8 +31,10 @@ if __name__ == "__main__":
     ghg_mmr_dict = dict()
     for ghg in GHG_MOLAR_MASS:
         ghg_mmr_dict[ghg] = load_cmip7_ghg_series_mmr(
-            args, "ScenarioMIP", ghg, CMIP7_SM_BEG_YEAR, CMIP7_SM_END_YEAR)
+            args, "ScenarioMIP", ghg, CMIP7_SM_BEG_YEAR, CMIP7_SM_END_YEAR
+        )
 
     # Patch the greenhouse gas namelist.
     cmip7_ghg_update_namelists_file(
-        ghg_mmr_dict, CMIP7_SM_BEG_YEAR, CMIP7_SM_END_YEAR)
+        ghg_mmr_dict, CMIP7_SM_BEG_YEAR, CMIP7_SM_END_YEAR
+    )
