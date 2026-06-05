392f84c152577e08950a91c2b4c7501ef3b15968
Paul Leopardi <paul.leopardi@anu.edu.au>
2026-06-05 11:30:42 +1000

    Refactor greenhouse gas forcings and add ScenarioMIP

--- CMIP7/esm1p6/atmosphere/cmip7_SM.py
+++ CMIP7/esm1p6/atmosphere/cmip7_SM.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 from cmip7_ancil_constants import ANCIL_TODAY
 
-CMIP7_SM_BEG_YEAR = 2025
+CMIP7_SM_BEG_YEAR = 2022
 # Model time interpolation requires an extra year
 CMIP7_SM_END_YEAR = 2100
 CMIP7_SM_NBR_YEARS = CMIP7_SM_END_YEAR + 1 - CMIP7_SM_BEG_YEAR
