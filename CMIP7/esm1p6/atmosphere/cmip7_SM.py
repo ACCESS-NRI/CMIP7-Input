a947a4bd822dc56a6f3684c07920922797d44bd9
Paul Leopardi <paul.leopardi@anu.edu.au>
2026-06-15 16:15:58 +1000

    Try setting CMIP7_SM_BEG_YEAR to 2023

--- CMIP7/esm1p6/atmosphere/cmip7_SM.py
+++ CMIP7/esm1p6/atmosphere/cmip7_SM.py
@@ -2,10 +2,8 @@ from pathlib import Path
 
 from cmip7_ancil_constants import ANCIL_TODAY
 
-CMIP7_SM_BEG_YEAR = 2022
-# Model time interpolation requires an extra year
+CMIP7_SM_BEG_YEAR = 2023
 CMIP7_SM_END_YEAR = 2100
-CMIP7_SM_NBR_YEARS = CMIP7_SM_END_YEAR + 1 - CMIP7_SM_BEG_YEAR
 
 
 def esm_sm_forcing_save_dirpath(args):
