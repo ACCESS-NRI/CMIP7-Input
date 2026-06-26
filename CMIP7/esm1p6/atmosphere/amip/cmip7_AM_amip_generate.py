6df2a0b681a1b64a98d5ff5c380315777a7f9887
Paul Leopardi <paul.leopardi@anu.edu.au>
2026-06-26 16:01:17 +1000

    Try setting gregorian to False in save_ancil

---
+++ CMIP7/esm1p6/atmosphere/amip/cmip7_AM_amip_generate.py
@@ -0,0 +1,60 @@
+from argparse import ArgumentParser
+from pathlib import Path
+
+from cmip7_ancil_argparse import (
+    grid_parser,
+    path_parser,
+)
+from cmip7_ancil_common import save_ancil
+from cmip7_ancil_constants import ANCIL_TODAY
+from cmip7_ancil_ukesm import (
+    fix_cmip7_ukesm,
+    load_cmip7_ukesm,
+    ukesm_parser,
+)
+
+
+def parse_args():
+    parser = ArgumentParser(
+        parents=[path_parser(), grid_parser(), ukesm_parser()],
+        prog="cmip7_AM_amip_generate",
+        description=(
+            "Generate input files from UK CMIP7 AMIP forcings"
+        ),
+    )
+    return parser.parse_args()
+
+
+def esm_am_amip_save_dirpath(args):
+    return (
+        Path(args.ancil_target_dirname)
+        / "modern"
+        / "amip"
+        / "atmosphere"
+        / "boundary_conditions"
+        / args.esm_grid_rel_dirname
+        / ANCIL_TODAY
+    )
+
+
+def save_cmip7_am_amip(args, cube):
+    # Save as an ancillary file
+    save_dirpath = esm_am_amip_save_dirpath(args)
+    save_ancil(
+        cube,
+        save_dirpath,
+        args.save_filename,
+        gregorian=False,
+        replace_bounds=True
+    )
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    # Load the CMIP7 datasets
+    ukesm_cube = load_cmip7_ukesm(args)
+    # Match the ESM1.5 mask
+    esm_cube = fix_cmip7_ukesm(args, ukesm_cube)
+    # Save the ancillary
+    save_cmip7_am_amip(args, esm_cube)
