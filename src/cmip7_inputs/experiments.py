a6503353b177c7a4a05a1b65012f232238317f0e
Davide Marchegiani <davide.marchegiani@anu.edu.au>
2026-08-06 16:06:13 +1000

    Added comments

--- src/cmip7_inputs/experiments.py
+++ src/cmip7_inputs/experiments.py
@@ -4,5 +4,11 @@ These are the values accepted by the ``--experiment``/``-e`` CLI
 option and used as keys when registering generators.
 """
 
-PI_CONTROL = "piControl"
+# List of all available experiments in the format:
+# <EXPERIMENT_KEY> = "<experiment_value>"
+# The <EXPERIMENT_KEY> is used when registering a generator function
+# with that experiment.
+# The <experiment_value> is the value accepted by the
+# `--experiment`/`-e` CLI option.
+PI_CONTROL = "picontrol"
 HISTORICAL = "historical"
--- src/cmip7_inputs/input_names.py
+++ src/cmip7_inputs/input_names.py
@@ -4,4 +4,10 @@ These are the values accepted by the ``--input_name``/``-n`` CLI
 option and used as keys when registering generators.
 """
 
+# List of all available input_names in the format:
+# <INPUT_NAME_KEY> = "<input_name_value>"
+# The <INPUT_NAME_KEY> is used when registering a generator function
+# with that input_name.
+# The <input_name_value> is the value accepted by the
+# `--input_name`/`-n` CLI option.
 SOLAR = "solar"
