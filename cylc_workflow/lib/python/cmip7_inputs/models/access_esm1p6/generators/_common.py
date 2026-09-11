8418e131feb1e21cf9cb2bc434a57a683b3f4541
mvgarcia <valentina260101@gmail.com>
2026-09-11 14:51:32 +1000

    Add solar historical into package

---
+++ cylc_workflow/lib/python/cmip7_inputs/models/access_esm1p6/generators/_common.py
@@ -0,0 +1,14 @@
+""" This contains all the common functions for generating the ESM1.5 ancil files. """
+
+from __future__ import annotations
+from argparse import Namespace
+
+from cmip7_inputs.core.context import GenerationRequest
+
+
+def cmip7_parse_args(request: GenerationRequest) -> Namespace:
+    '''
+    Parse the command line arguments for CMIP7 historical
+    solar ancil file generation.
+    '''
+    return Namespace(**request.options)
