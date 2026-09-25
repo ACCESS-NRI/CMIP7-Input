59bc53ed1b6fa78b4a73e10a984953ba4c762a4a
Paul Leopardi <paul.leopardi@anu.edu.au>
2026-09-25 13:27:27 +1000

    Fix RTD canonical URL, edit URI, and link checker configuration

--- documentation/hooks/hide_pages_url_segment.py
+++ documentation/hooks/hide_pages_url_segment.py
@@ -1,11 +1,20 @@
-# Change the URL segment for pages in MkDocs to hide the "/pages" prefix, so
-# that pages inside the "pages" directory are served directly at the root URL.
+import os
 from mkdocs.plugins import event_priority
 
 
 @event_priority(-100)
+def on_files(files, config):
+    for file in files:
+        if file.src_uri.startswith("pages/"):
+            file.dest_uri = file.dest_uri.removeprefix("pages/")
+            file.dest_path = file.dest_path.removeprefix("pages/")
+            file.url = file.url.removeprefix("pages/")
+            if not file.url:
+                file.url = "./"
+            file.abs_dest_path = os.path.normpath(os.path.join(file.dest_dir, file.dest_uri))
+    return files
+
+
+@event_priority(-100)
 def on_page_markdown(markdown, *, page, config, files):
-    if page.file.url.startswith("pages/"):
-        page.file.url = page.file.url.removeprefix("pages/")
-        page.file.dest_uri = page.file.dest_uri.removeprefix("pages/")
-        page.file.abs_dest_path = page.file.abs_dest_path.removeprefix("pages/")
+    page.canonical_url = (config.site_url or "") + page.file.url
